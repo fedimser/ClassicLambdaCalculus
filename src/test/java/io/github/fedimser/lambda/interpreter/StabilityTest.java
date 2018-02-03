2afa2a0e0e6ee1d80b993c4fdcfa0657818c35a5
Dmytro Fedoriaka <fedimser@yandex.ru>
2018-02-03 19:05:02 +0300

    Added tests for stability.

--- src/test/java/io/github/fedimser/lambda/interpreter/StabilityTest.java
+++ src/test/java/io/github/fedimser/lambda/interpreter/StabilityTest.java
@@ -5,17 +5,31 @@ import io.github.fedimser.lambda.calculus.LbdExpression.FormulaStyle;
 import io.github.fedimser.lambda.interpreter.LambdaInterpreter;
 import junit.framework.TestCase;
 
+import java.beans.Expression;
+
 public class StabilityTest extends TestCase {
     private LambdaInterpreter li = new LambdaInterpreter();
 
-    // Checks that both expression are reduced to the same expression.
-    private void check(String exp1, String exp2) throws Exception {
-        assertTrue(li.evaluate(exp1).equals(li.evaluate(exp2)));
+    // First, evaluates given expession.
+    // Than gets its classic formula.
+    // Than parses it and gets classic formula for new expression.
+    // Checks that given string is classic formula of expression it describes.both formulas are the same.
+    private void check(String... formulas) throws Exception {
+        for(String f : formulas) {
+            LbdExpression ex1 = li.evaluate(f);
+            String s1 = ex1.getFormula(FormulaStyle.CLASSIC);
+            LbdExpression ex2 = li.evaluate(s1);
+            String s2 = ex2.getFormula(FormulaStyle.CLASSIC);
+            assertEquals(s1, s2);
+        }
     }
 
-
-    private void checkStableClassic(String s) throws Exception {
-        assertEquals(s, li.evaluate(s).getFormula(FormulaStyle.CLASSIC));
+    public void testStability() throws Exception {
+        check("ID", "SUM", "MUL", "SUB", "INC", "PRED", "POW");
+        check("TRUE", "FALSE", "NOT", "AND", "OR");
+        check("IS_ZERO", "GTE", "LTE", "GT", "LT", "EQUALS");
+        check("PAIR", "FIRST", "SECOND");
+        check("FOR", "FACTORIAL");
     }
 
 }
